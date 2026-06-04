e2172353790d739f15a0139549bc1162670e67ff
acuicolasvientoenpopa-coder <acuicolasvientoenpopa@gmail.com>
2026-06-04 13:22:06 -0600

    Update sw[1].js

--- sw[1].js
+++ sw[1].js
@@ -5,16 +5,22 @@ self.addEventListener('install',e=>e.waitUntil(
     '/index.html'
   ])).then(()=>self.skipWaiting())
 ));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(
-  caches.match(e.request).then(cached=>{
-    if(cached) return cached;
-    return fetch(e.request).then(res=>{
-      if(!res||res.status!==200) return res;
-      return caches.open(CACHE).then(cache=>{
-        cache.put(e.request,res.clone());
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )).then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached=>{
+      if(cached) return cached;
+      return fetch(e.request).then(res=>{
+        if(!res||res.status!==200||res.type==='opaque') return res;
+        const clone=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,clone));
         return res;
       });
-    }).catch(()=>caches.match('/index.html'));
-  })
-));
+    }).catch(()=>caches.match('/index.html'))
+  );
+});
